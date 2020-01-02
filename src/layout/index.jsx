1fb33b3bbc8ecd19c2725584b86fdb4efb9724f4
Laura Horackova <39065215+elaidina@users.noreply.github.com>
2020-01-02 22:40:20 +0100

    m

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Navigation from '../components/navigation';
-import ThemeContext from '../context';
-import './style/style.scss';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Navigation from "../components/navigation";
+import ThemeContext from "../context";
+import "./style/style.scss";
+import CookieConsent from "react-cookie-consent";
 
 const Layout = ({ children }) => {
   let websiteTheme;
@@ -25,12 +26,15 @@ const Layout = ({ children }) => {
       <Navigation>
         <main className="page-wrap">{children}</main>
       </Navigation>
+      <CookieConsent>
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </ThemeContext.Provider>
   );
 };
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
